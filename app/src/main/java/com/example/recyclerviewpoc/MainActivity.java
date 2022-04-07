95f3c30458c5a2dcf820ba7ea3c5d6e7671ad7eb
b21sebgr <102599253+b21sebgr@users.noreply.github.com>
2022-04-07 09:46:40 +0200

    Added data and handling of the binding from layout to recyclingview in code

--- app/src/main/java/com/example/recyclerviewpoc/MainActivity.java
+++ app/src/main/java/com/example/recyclerviewpoc/MainActivity.java
@@ -1,14 +1,25 @@
 package com.example.recyclerviewpoc;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        List<String> mountains = Arrays.asList("Kinnekulle", "Billingen", "K2", "Kebenikaise",
+                                               "Hanneberg", "Hunneberg");
+
+        recyclerView = findViewById(R.id.my_recycler_view);
     }
 }
